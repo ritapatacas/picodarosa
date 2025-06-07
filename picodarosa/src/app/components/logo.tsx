d7ba0e8eb6f9054b09ac218ad4a8d478b6c995e1
ritapatacas <ritapatacas@users.noreply.github.com>
2025-06-08 00:24:45 +0100

    first commit

---
+++ picodarosa/src/app/components/logo.tsx
@@ -0,0 +1,22 @@
+// components/Logo.tsx
+
+export default function Logo() {
+    return (
+      <svg
+        width="150"
+        height="150"
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10 10 H 90 V 90 H 10 Z"
+          stroke="black"
+          strokeWidth="2"
+          fill="white"
+        />
+        {/* Ajusta conforme o teu vetor */}
+      </svg>
+    );
+  }
+  
